fbcea574ce788a59c2b484c26204964344174b85
shiwen <shiwen@4399.com>
2024-01-20 15:28:09 +0800

    day08 实现 useEffect

--- App.jsx
+++ App.jsx
@@ -1,5 +1,10 @@
 import React from './core/React.js'
 
+/**
+ * useEffect
+ * 调用时机是在 React 完成对 DOM渲染之后，并且浏览器完成绘制之前
+ */
+
 function Foo() {
     const [count, setCount] = React.useState(10)
     const [bar, setBar] = React.useState('bar')
@@ -7,6 +12,30 @@ function Foo() {
         setCount((c) => c + 1)
         setBar('bar')
     }
+
+    React.useEffect(() => {
+        console.log('init')
+
+        return () => {
+            console.log('cleanup 0')
+        }
+    }, [])
+
+    React.useEffect(() => {
+        console.log('update', count)
+
+        return () => {
+            console.log('cleanup 1')
+        }
+    }, [count])
+
+    React.useEffect(() => {
+        console.log('update', count)
+
+        return () => {
+            console.log('cleanup 2')
+        }
+    }, [count])
 
     return (
         <div>
